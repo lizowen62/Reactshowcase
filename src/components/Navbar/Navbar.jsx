ccf5b1641a3ad76640b03dd789d9ec298a35cd6d
gabriel <motta.gabriel51@gmail.com>
2022-03-03 21:48:29 +0100

    pushing new src

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Navbar.css';
+
+const Navbar = () => (
+  <nav className='app__navbar'>
+    <div>
+      <p> hello </p>
+    </div>
+    <ul className='app__navbar-links'>
+      <li className='app__opensans'> home </li>
+      <li className='app__opensans'> about </li>
+      <li className='app__opensans'> home </li>
+      <li className='app__opensans'> home </li>
+      <li className='app__opensans'> home </li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
